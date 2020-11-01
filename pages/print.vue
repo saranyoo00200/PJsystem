c7559aca7eb78f874cab135f21ee88b38a3fd051
saranyoo00200 <saranyoo00200@gmail.com>
2020-11-01 17:12:06 +0700

    first commit

---
+++ pages/print.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click="print">
+      Print this component
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
